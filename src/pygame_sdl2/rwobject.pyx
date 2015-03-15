c77162d3e67d3c92f9f0f1e9255108f027aa6320
Tom Rothamel <tom@rothamel.us>
2015-03-15 00:19:38 -0400

    Fix encoding issues in rwops.
    All file operations now go through SDL_RWFromFile, which takes a
    utf-8 filename as an argument.

--- src/pygame_sdl2/rwobject.pyx
+++ src/pygame_sdl2/rwobject.pyx
@@ -27,6 +27,25 @@ import sys
 # The fsencoding.
 fsencoding = sys.getfilesystemencoding() or "utf-8"
 
+cdef extern from "SDL.h" nogil:
+    Sint64 SDL_RWtell(SDL_RWops* context)
+
+    Sint64 SDL_RWseek(SDL_RWops* context,
+                      Sint64     offset,
+                      int        whence)
+
+    size_t SDL_RWread(SDL_RWops* context,
+                  void*             ptr,
+                  size_t            size,
+                  size_t            maxnum)
+
+    size_t SDL_RWwrite(SDL_RWops* context,
+                       const void*       ptr,
+                       size_t            size,
+                       size_t            num)
+
+    int SDL_RWclose(SDL_RWops* context)
+
 
 cdef set_error(e):
     cdef char *msg
@@ -97,7 +116,7 @@ cdef int python_close(SDL_RWops *context) with gil:
     return 0
 
 cdef struct SubFile:
-    FILE *f
+    SDL_RWops *rw
     Sint64 base
     Sint64 length
 
@@ -109,18 +128,16 @@ cdef Sint64 subfile_seek(SDL_RWops *context, Sint64 seek, int whence) nogil:
     cdef SubFile *sf = <SubFile *> context.hidden.unknown.data1
 
     if whence == RW_SEEK_SET:
-        fseek(sf.f, seek + sf.base, SEEK_SET)
+        return SDL_RWseek(sf.rw, seek + sf.base, RW_SEEK_SET)
     elif whence == RW_SEEK_CUR:
-        fseek(sf.f, seek, SEEK_CUR)
+        return SDL_RWseek(sf.rw, seek, RW_SEEK_CUR)
     elif whence == RW_SEEK_END:
-        fseek(sf.f, sf.base + sf.length + seek, SEEK_END)
-
-    return ftell(sf.f) - sf.base
+        return SDL_RWseek(sf.rw, sf.base + sf.length + seek, SEEK_END)
 
 cdef size_t subfile_read(SDL_RWops *context, void *ptr, size_t size, size_t maxnum) nogil:
     cdef SubFile *sf = <SubFile *> context.hidden.unknown.data1
 
-    cdef Sint64 tell = ftell(sf.f) - sf.base
+    cdef Sint64 tell = SDL_RWtell(sf.rw) - sf.base
     cdef Sint64 left = sf.length - tell
 
     if size * maxnum > left:
@@ -129,41 +146,42 @@ cdef size_t subfile_read(SDL_RWops *context, void *ptr, size_t size, size_t maxn
     if maxnum == 0:
         return 0
 
-    return fread(ptr, size, maxnum, sf.f)
+    return SDL_RWread(sf.rw, ptr, size, maxnum)
 
 cdef int subfile_close(SDL_RWops *context):
     cdef SubFile *sf = <SubFile *> context.hidden.unknown.data1
 
-    fclose(sf.f)
+    SDL_RWclose(sf.rw)
     free(sf)
     SDL_FreeRW(context)
 
 
 cdef SDL_RWops *to_rwops(filelike, mode="rb") except NULL:
 
+    cdef FILE *f
+    cdef SubFile *sf
     cdef SDL_RWops *rv
+    cdef SDL_RWops *rw
 
     if isinstance(filelike, file) and mode == "rb":
+        print "Opening file with name", repr(filelike.name)
         filelike = filelike.name
 
     # Try to open as a file.
     if isinstance(filelike, str):
-        name = filelike
+        name = filelike.decode(fsencoding)
     elif isinstance(filelike, unicode):
-        name = filelike.encode(fsencoding)
+        name = filelike
     else:
         name = None
 
     if name:
-        rv = SDL_RWFromFile(name, mode)
+        rv = SDL_RWFromFile(name.encode("utf-8"), mode)
 
         if rv == NULL:
             raise IOError("Could not open {!r}: {}".format(filelike, SDL_GetError()))
 
         return rv
-
-    cdef FILE *f
-    cdef SubFile *sf
 
     if mode == "rb":
         try:
@@ -176,14 +194,18 @@ cdef SDL_RWops *to_rwops(filelike, mode="rb") except NULL:
 
             if name is not None:
 
-                f = fopen(name, "rb")
-                if not f:
-                    raise IOError("Could not open {}.".format(name))
+                if not isinstance(name, unicode):
+                    name = name.decode(fsencoding)
 
-                fseek(f, base, SEEK_SET)
+                rw = SDL_RWFromFile(name.encode("utf-8"), "rb")
+
+                if not rw:
+                    raise IOError("Could not open {!r}.".format(name))
+
+                SDL_RWseek(rw, base, RW_SEEK_SET);
 
                 sf = <SubFile *> calloc(sizeof(SubFile), 1)
-                sf.f = f
+                sf.rw = rw
                 sf.base = base
                 sf.length = length
 
@@ -218,5 +240,6 @@ cdef SDL_RWops *to_rwops(filelike, mode="rb") except NULL:
 
     return rv
 
-cdef api SDL_RWops *RWopsFromPython(filelike):
+
+cdef api SDL_RWops *RWopsFromPython(filelike) except NULL:
     return to_rwops(filelike)
