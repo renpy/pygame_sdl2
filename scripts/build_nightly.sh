388d938f272b0b1ff943a0a8239bbd50ced83afc
Tom Rothamel <tom@rothamel.us>
2016-01-24 13:15:37 -0500

    Uncomment the useful parts.

--- scripts/build_nightly.sh
+++ scripts/build_nightly.sh
@@ -8,16 +8,16 @@ DATE=$(date +%Y.%m.%d)
 DATE2=$(date +%Y-%m-%d)
 WWW=/home/tom/magnetic/ab/WWW.nightly
 
-# rm -Rf "/home/tom/ab/$PROJECT" || true
-# git clone https://github.com/renpy/pygame_sdl2 "/home/tom/ab/$PROJECT"
+rm -Rf "/home/tom/ab/$PROJECT" || true
+git clone https://github.com/renpy/pygame_sdl2 "/home/tom/ab/$PROJECT"
 
 cd "/home/tom/ab/$PROJECT"
 
-# git clone https://github.com/renpy/pygame_sdl2_windeps
-# "$SCRIPT/build_all.sh" $PROJECT a$DATE
+git clone https://github.com/renpy/pygame_sdl2_windeps
+"$SCRIPT/build_all.sh" $PROJECT a$DATE
 
-# cp $WWW/current/*-$DATE2-*-rapt.zip dist/android.zip || true
-# cp $WWW/current/*-$DATE2-*-renios.zip dist/ios.zip || true
+cp $WWW/current/*-$DATE2-*-rapt.zip dist/android.zip || true
+cp $WWW/current/*-$DATE2-*-renios.zip dist/ios.zip || true
 
 D="$WWW/pygame_sdl2/nightly-$DATE2"
 
@@ -26,7 +26,7 @@ mkdir -p $D
 cp -a dist/* $D
 
 rm "$WWW/pygame_sdl2/current" || true
-ln -s $D "$WWW/pygame_sdl2/current"
+ln -s "nightly-$DATE2" "$WWW/pygame_sdl2/current"
 
 # Upload everything to the server.
 rsync -av /home/tom/magnetic/ab/WWW.nightly/pygame_sdl2/ tom@erika.onegeek.org:/home/tom/WWW.nightly/pygame_sdl2 --delete
