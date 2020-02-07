c179506b6ef47c06bb423551f70c66d7b79fef06
edumudu <eduardomudutiu@gmail.com>
2020-02-07 00:51:50 -0300

    Changed the way to write a variable on the page in php to a simpler and shorter form

--- test/util/build_page/warnings.php
+++ test/util/build_page/warnings.php
@@ -1,5 +1,5 @@
 <?php
-$build_tag = $_GET['tag'];
+$build_tag = $_GET['tag'] ?? '';
 $results_file = "buildresults_".$build_tag.".txt";
 $result_contents = explode("\n", file_get_contents($results_file));
 
@@ -8,14 +8,14 @@ $result_contents = explode("\n", file_get_contents($results_file));
 <html>
 
  <head>
-  <title>Alert! Warnings for <? echo $build_tag; ?> Automated Pygame Build Page</title>
+  <title>Alert! Warnings for <?= $build_tag; ?> Automated Pygame Build Page</title>
  </head>
 
 <body>
 
- <h2>Alert! Warnings for <? echo $build_tag; ?> Automated Pygame Build Page</h2>
+ <h2>Alert! Warnings for <?= $build_tag; ?> Automated Pygame Build Page</h2>
  <hr>
- <p>Below are the <a href="results/<?php echo "$build_tag/$result_contents[0]/"?>setup">build</a> warnings for the <? echo $build_tag; ?> build from the <a href="index.php">Spectacularly Adequate Automated Pygame Build Page</a>. Although most warnings are related to casting or types and are harmless, they may indicate a bug or error in the build.</p> 
+ <p>Below are the <a href="results/<?= "$build_tag/$result_contents[0]/"?>setup">build</a> warnings for the <?= $build_tag; ?> build from the <a href="index.php">Spectacularly Adequate Automated Pygame Build Page</a>. Although most warnings are related to casting or types and are harmless, they may indicate a bug or error in the build.</p> 
  <hr>
 
  <table width=100% style='border-top:1px solid #006c03; background:#DDDD44;'>
@@ -25,13 +25,13 @@ $result_contents = explode("\n", file_get_contents($results_file));
  </table>
  <table width=100% border=1>
   <tr>
-   <td><b><? echo $build_tag; ?></b></td>
+   <td><b><?= $build_tag; ?></b></td>
   </tr>
   <tr>
-   <td><p><? echo "Revision: " . $result_contents[0] . " Built: " . $result_contents[1]; ?></td>
+   <td><p><?= "Revision: " . $result_contents[0] . " Built: " . $result_contents[1]; ?></td>
   </tr>
   <tr>
-   <td><p><? echo $result_contents[4]; ?></td>
+   <td><p><?= $result_contents[4]; ?></td>
   </tr>
  </table>
  
